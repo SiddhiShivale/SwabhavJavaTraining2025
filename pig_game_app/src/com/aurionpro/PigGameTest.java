d336bb31dc7a6c04f593faecd4dce45159ca835a
SiddhiShivale <134221149+SiddhiShivale@users.noreply.github.com>
2025-07-10 16:38:00 +0530

    Update and rename pig_game_app/src/com/aurionpro/test/App.java to pig_game_app/src/com/aurionpro/PigGameTest.java

---
+++ pig_game_app/src/com/aurionpro/PigGameTest.java
@@ -0,0 +1,13 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.PigDiceGame;
+
+public class PigGameTest {
+
+	public static void main(String[] args) {
+		PigDiceGame game = new PigDiceGame();
+		
+		game.startGame();
+	}
+
+}
